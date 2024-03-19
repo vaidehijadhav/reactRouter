471e8e818fe1e79b8d7d1008945594907f51f42c
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-03-19 18:22:23 +0530

    write routes for different pages

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<div>Home Page</div>} />
+        <Route path="/support" element={<div>Support Page</div>} />
+        <Route path="/about" element={<div>About Page</div>} />
+        <Route path="/labs" element={<div>Labs Page</div>} />
+        <Route path="*" element={<div>Page Not Found</div>} />
       </Routes>
     </div>
   );
